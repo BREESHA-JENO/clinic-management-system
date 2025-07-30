b4b3b3b4064857239ef598f85d3a7d5235a4e523
Breesha <breesha.rs@gmail.com>
2025-07-30 16:14:52 +0530

    committed

--- backend/server.js
+++ backend/server.js
@@ -28,5 +28,6 @@ app.use('api/pharmacist',pharmacistRouter)
 connectDB().then(()=>{
     app.listen(PORT,()=>{
         console.log(`Server running on port ${PORT}`);
+        console.log("hii")
     });
 });
